72ead11c5ee956ec0d7997ebd3086390019ce0a3
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-10-25 09:50:33 +0200

    25102022

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ db = SQLAlchemy(app)
 
 menu = [{'name': 'Über die Webseite', 'url': '/about'},
         {'name': 'Projekte', 'url': '/projects'},
+        {'name': 'Technologie', 'url': '/technology'},
         {'name': 'Tagebuch', 'url': '/diary'},
         {'name': 'Anmeldung', 'url': '/login'}]
 
@@ -32,11 +33,11 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    link = db.Column(db.String(300), nullable=True)
-    date = db.Column(db.DateTime, nullable=False)
+    link = db.Column(db.String(300), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return 'Post %r' % self.id
+        return '<Post %r>' % self.id
 
 
 class Project(db.Model):
@@ -46,10 +47,21 @@ class Project(db.Model):
     link_github = db.Column(db.String(300), nullable=True)
     link_video = db.Column(db.String(300), nullable=True)
     link_other = db.Column(db.String(300), nullable=True)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return 'Project %r' % self.id
+        return '<Project %r>' % self.id
+
+
+class Technology(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    logo = db.Column(db.String(300), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    link = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return '<Technology %r>' % self.id
 
 
 @app.route('/')
@@ -68,11 +80,16 @@ def projects():
     return render_template('projects.html', title='Projekte', menu=menu)
 
 
+@app.route('/technology')
+def technology():
+    technologies = Technology.query.all()
+    return render_template('technology.html', title='Technology', technologies=technologies, menu=menu)
+
+
 @app.route('/diary')
-def posts():
-    return render_template('diary.html', title='Tagebuch', menu=menu)
-    #posts = Post.query.order_by(Post.date).all()
-    #return render_template('diary.html', posts=posts)
+def diary():
+    posts = Post.query.order_by(Post.date).all()
+    return render_template('diary.html', title='Tagebuch', posts=posts, menu=menu)
 
 
 @app.errorhandler(404)
@@ -91,23 +108,62 @@ def login():
     return render_template('login.html', title='Anmeldung', menu=menu)
 
 
+@app.route('/new-technology', methods=['POST', 'GET'])
+def new_technology():
+    if request.method == "POST":
+        title = request.form['title']
+        logo = request.form['logo']
+        description = request.form['description']
+        link = request.form['link']
+
+        technology = Technology(title=title, logo=logo, description=description, link=link)
+        print('Technologie wurde gespeichert')
+        try:
+            db.session.add(technology)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print('Except')
+            return "Es ist ein Fehler aufgetreten."
+
+    else:
+        print('Technologie wurde nicht gespeichert')
+        return render_template('new_technology.html', title='Neue Technologie', menu=menu)
+
+
 @app.route('/new-post', methods=['POST', 'GET'])
-def create_post():
+def new_post():
     if request.method == "POST":
         title = request.form['title']
         text = request.form['text']
         link = request.form['link']
 
         post = Post(title=title, text=text, link=link)
+        print('Post wurde gespeichert')
         try:
             db.session.add(post)
             db.session.commit()
             return redirect('/')
         except:
+            print('Except')
             return "Es ist ein Fehler aufgetreten."
-
     else:
+        print('Post wurde nicht gespeichert')
         return render_template('new_post.html', title='Neues Post', menu=menu)
+
+
+#def christmas():
+#    today = datetime.today()
+#    christmas_day = datetime(2022, 12, 24)
+#    delta = christmas_day - today
+#    return delta
+
+
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
